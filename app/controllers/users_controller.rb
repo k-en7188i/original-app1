9963f20cd1c32cd2612c7f41042ce0b47020a7d1
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-05-01 12:06:38 +0900

    マイページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    # @tweets = user.tweets
-    # @post = user.posts
+    def show
+      user = User.find(params[:id])
+      @nickname = user.nickname
+      @posts = user.posts
+    end
   end
 end
