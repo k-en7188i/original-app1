c5baf7fae1d8023f84c3db2a0e0bd4bffaa7d396
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-05-05 17:32:18 +0900

    いいね機能の実装準備

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,17 @@ class Post < ApplicationRecord
   belongs_to :category
   
   belongs_to :user
-  
+  has_many :likes
+
   has_one_attached :image
 
 
   validates :title, :content, :urllink, presence: true 
   #ジャンルの選択が「---」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
-  resources :posts, only: [:index, :new, :create, :destroy, :show, :edit, :update]
+  resources :posts, only: [:index, :new, :create, :destroy, :show, :edit, :update] do
+    resources :likes, only: [:create, :destroy]
+  end
+
 
   resources :users, only: :show
 end
