41868ea7c33fe9e7d7d851e0131696d13976b1ef
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-05-05 18:57:22 +0900

    いいね機能の実装２

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
   resources :posts, only: [:index, :new, :create, :destroy, :show, :edit, :update] do
-    resources :likes, only: [:create, :destroy]
+    resource :likes, only: [:create, :destroy]
   end
 
 
