2303a8f1f80f8106802de903b7eeedf1e47ac5bf
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-05-10 18:39:17 +0900

    コメント画面の設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
   resources :posts, only: [:index, :new, :create, :destroy, :show, :edit, :update] do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy]
     resource :likes, only: [:create, :destroy]
     collection do
       get 'search'
