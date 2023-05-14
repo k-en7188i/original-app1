ac0358e31866d5bc878939d719cdb3ca39406bc9
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-05-14 13:43:17 +0900

    usermodelへのバリデーション記述の準備

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,27 @@
 class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-
   belongs_to :user
-  has_many :comments  # commentsテーブルとのアソシエーション
+
+  # commentsテーブルとのアソシエーション
+  has_many :comments  
+  
   # いいね機能
   has_many :likes
-# お気に入り機能
+
+  # お気に入り機能
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
 
+  # ActiveStorageの記述
   has_one_attached :image
 
+  # バリデーションの記述
   validates :title, :content, :urllink, presence: true
   # ジャンルの選択が「---」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
 
+  # いいね機能
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
   end
@@ -23,7 +29,8 @@ class Post < ApplicationRecord
   def likes_count
     likes.count
   end
-
+  
+  # 検索機能
   def self.search(search)
     if search != ''
       Post.where('title LIKE(?)', "%#{search}%")
