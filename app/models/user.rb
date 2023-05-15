79f2e023e61823a9296612da018321986a8b381b
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-05-15 20:06:06 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts
-  has_many :comments  # commentsテーブルとのアソシエーション
-  has_many :likes
+  # ユーザー登録時の性別選択プルダウン
 
-  has_many :favorites, dependent: :destroy
-  has_many :posts, through: :favorites
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :gender
+    
+
+    has_many :posts
+    has_many :comments  # commentsテーブルとのアソシエーション
+    has_many :likes
+
+    has_many :favorites, dependent: :destroy
+
+    has_many :favorite_posts, through: :favorites, source: :post
+
+    # バリデーションの記述
+    validates :nickname, presence: true
+    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :birth_date, presence: true
+    validates :gender, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+
+
+
 end
