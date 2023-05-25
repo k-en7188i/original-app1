82257433f678a8b675cbae97c957b20329f80d19
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-05-25 23:29:59 +0900

    postController modificate
    for  preparation of incremental search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,14 +9,12 @@ class PostsController < ApplicationController
 
     @user = current_user # サインインしているユーザーを取得するために、current_userを使用します
 
-    # ランキング機能
-    # @ranking_posts = Post.ranking.limit(10)  # ランキング上位10件を取得
   end
-
+  
   def new
     @post = Post.new
   end
-
+  
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -26,46 +24,63 @@ class PostsController < ApplicationController
       render :new
     end
   end
-
+  
   def show
     @posts = Post.all.includes(:user)
     @user = @post.user
     # @posts = Post.all.includes(:user)
-
+    
     @comment = Comment.new
     @comments = @post.comments.includes(:user)
-
+    
     @prev_post = Post.where('id < ?', @post.id).order('id DESC').first
     @next_post = Post.where('id > ?', @post.id).order('id ASC').first
   end
-
+  
   def destroy
     @post.destroy
   end
-
+  
   def edit
   end
-
+  
   def update
     @post.update(post_params)
   end
-
+  
   # 検索機能
   def search
     @posts = Post.search(params[:title])
   end
-
+  
+  # インクリメントサーチ機能
+  # json形式で検索結果データを返すので完全が必要
+  # def search
+  #   return nil if params[:title] == ""
+  #   posts = Post.where(['title LIKE ?', "%#{params[:title]}%"])
+  #   render json: posts
+  # end
+  
+  def check_read
+    user_id = params[:user_id]
+    post_id = params[:post_id]
+    
+    read = Check.where(user_id: user_id, post_id: post_id).exists?
+    render json: { read: read }
+  end
+  
   # ランキング機能
   def ranking
     @posts = Post.ranking
+    # @posts = Post.ranking.limit(8)  # ランキング上位8件を取得
   end
-
+  
   private
-
+  
   def set_item
     @post = Post.find(params[:id])
   end
-
+  
   def post_params
     params.require(:post).permit(:title, :content, :urllink, :category_id, :image).merge(user_id: current_user.id)
   end
