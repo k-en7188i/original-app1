8092507f898f8820498142c9b60cf627c171a418
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-05-26 15:19:59 +0900

    Feature:Follow -add relationship-association

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,21 @@ class User < ApplicationRecord
   has_many :comments # commentsテーブルとのアソシエーション
   has_many :likes
 
-  # ランキング機能
-  # has_many :liked_posts, through: :likes, source: :post
-
+  # お気に入り機能
   has_many :favorites, dependent: :destroy
-
   has_many :favorite_posts, through: :favorites, source: :post
+
+  # 既読機能
+  has_many :checks, dependent: :destroy
+  has_many :posts, through: :checks
+
+  # フォロー機能
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
 
   # ActiveStorageの記述/ユーザー登録時の画像投稿準備
   has_one_attached :image_profile
@@ -28,4 +37,10 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :birth_date, presence: true
   validates :gender_id, presence: true, numericality: { other_than: 1, message: '『 --- 』以外を選択してください' }
+
+  # フォロー機能
+  def followed_by?(user)
+    follower =  passive_relationships.find_by(following_id: user.id)
+    return follower.present?
+  end
 end
