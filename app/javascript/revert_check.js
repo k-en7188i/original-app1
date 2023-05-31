b61ab7ebebd08e1b8b30d6ad3d78a8a138b1ceca
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-05-31 09:35:37 +0900

    Create revert_check.js

---
+++ app/javascript/revert_check.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', function() {
+  console.log("読み込み完了");
+  const articleCheckButtons = document.querySelectorAll(".article-check-button");
+  articleCheckButtons.forEach(function(button) {
+    console.log("読み込み完了?");
+    button.addEventListener("click", () => {
+      const postId = button.getAttribute("data-post-id");
+      const deleteFlg = window.confirm('記事の既読を削除しますか？');
+      if (deleteFlg) {
+        const XHR = new XMLHttpRequest();
+        XHR.open("GET", `/checks/revert/${postId}`, true);
+        XHR.send();
+        XHR.onload = () => {
+          button.parentNode.remove();
+        };
+      } else {
+        // 削除しない場合の処理
+      }
+    });
+  });
+});
