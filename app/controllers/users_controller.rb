cb22025eda749e8841592ebe7a9e306c4929e6fc
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-06-10 13:30:07 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    @nickname = @user.nickname
-    @posts = @user.posts
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @user_posts = user.posts
   end
 
   def favorites
