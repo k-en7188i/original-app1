b4abdd561bc5fe17eb7c78bd781a83523edf6f46
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-06-11 16:09:57 +0900

    Update post.rb : Advance preparation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   belongs_to :category
   belongs_to :user
 
+  has_many :reviews, dependent: :destroy # reviewsテーブルとのアソシエーション
+
   # commentsテーブルとのアソシエーション
   has_many :comments
 
@@ -48,6 +50,24 @@ class Post < ApplicationRecord
       Post.where('title LIKE(?)', "%#{search}%")
     else
       Post.all
+    end
+  end
+
+
+  # レビュー機能
+  def avg_score
+    unless self.reviews.empty?
+      reviews.average(:score).round(1).to_f
+    else
+      0.0
+    end
+  end
+
+  def review_score_percentage
+    unless self.reviews.empty?
+      reviews.average(:score).round(1).to_f*100/5
+    else
+      0.0
     end
   end
 end
