283eefc4327856eeaf8a985e4d830467958b7739
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-06-12 14:02:27 +0900

    Create reviews_controller.rb

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,32 @@
+class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+  def index
+    @review = Review.new
+    @post = Post.find(params[:post_id])
+    @reviews = @post.reviews.includes(:user)
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @review = @post.reviews.build(review_params)
+    @review.user_id = current_user.id
+
+    if @review.save
+      redirect_to post_reviews_path(@post), notice: "Review created successfully."
+    else
+      render "posts/show"
+    end
+  end
+
+  # レビューする画面
+  def show
+    @post = Post.find(params[:post_id])
+    @review = Review.new
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:score, :content)
+  end
+end
