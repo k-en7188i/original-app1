7601f325f455084b4e5ad63a5ff6c8c75f4f180d
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-06-18 13:00:20 +0900

    Update tag.js

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -1,24 +1,26 @@
-// インクリメントサーチの実装
-
 document.addEventListener("DOMContentLoaded", () => {
   const searchInput = document.querySelector("#post_search");
   console.log("searchInputスタート");
-  if (searchInput){
+  if (searchInput) {
     const inputElement = document.getElementById("post_search");
+    const searchResult = document.getElementById("search-result");
     inputElement.addEventListener("input", () => {
-      const keyword = document.getElementById("post_search").value;
+      const keyword = inputElement.value;
+      if (keyword.trim() === "") {
+        searchResult.innerHTML = "";
+        return;
+      }
       fetch(`/posts/search?title=${keyword}`)
-        .then(response => response.text())
-        .then(data => {
-          const searchResult = document.getElementById("search-result");
+        .then((response) => response.text())
+        .then((data) => {
           searchResult.innerHTML = data;
-          searchResult.querySelectorAll('.child').forEach(childElement => {
+          searchResult.querySelectorAll(".child").forEach((childElement) => {
             childElement.addEventListener("click", () => {
-              document.getElementById("post_search").value = childElement.textContent;
+              inputElement.value = childElement.textContent;
               childElement.remove();
             });
           });
         });
     });
-  };
+  }
 });
